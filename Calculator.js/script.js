cd40171309e0f24c0f67f3757601e15a693b7a4e
Souraj <sourajg1@gmail.com>
2023-01-29 14:12:39 +0530

    js added

--- Calculator.js/script.js
+++ Calculator.js/script.js
@@ -1,2 +1,31 @@
-let display = document.getElementById(screen);
-buttons = document.querySelectorAll(button);
+const screen = document.getElementById('screen');
+const buttons = document.querySelectorAll('button');
+const del = document.querySelector('de');
+let screenValue = '' ;
+for (items of buttons){
+    items.addEventListener("click", (x)=>{
+        buttonText = x.target.innerText ;
+        if (buttonText == 'x') {
+            buttonText= '*' ;
+            screenValue +=  buttonText ;
+            screen.value = screenValue ;
+        } 
+         else if(buttonText == 'AC') {
+            screenValue =  "" ;
+            screen.value = screenValue ;
+        }
+        else if(buttonText == '=') {
+            screen.value = eval(screenValue) ;
+        }
+        else if(buttonText == 'DE'){
+            screenValue = screen.innerText.slice(0,-1)  ;
+            screen.value = screenValue ;
+        }
+      else {
+        screenValue +=  buttonText ;
+        screen.value = screenValue ;
+      }
+
+       })
+}
+
